ff354d76f9b84a77ac3e694576a48db474bd93eb
hongvan510 <tranhongvan2003pq@gmail.com>
2025-04-04 17:00:36 +0700

    push code new

--- Project_QLTS_DNC/View/NhaCungCap/NhaCungCapForm.xaml.cs
+++ Project_QLTS_DNC/View/NhaCungCap/NhaCungCapForm.xaml.cs
@@ -10,14 +10,41 @@ namespace Project_QLTS_DNC.View.NhaCungCap
     /// </summary>
     public partial class NhaCungCapForm : UserControl
     {
-
         public NhaCungCapForm()
         {
             InitializeComponent();
-           
+            LoadData();  // Gọi LoadData khi UserControl được khởi tạo
+        }
+
+        // Định nghĩa lớp NhaCungCapModel với các thuộc tính public
+        public class NhaCungCapModel
+        {
+            public string MaNCC { get; set; }  // Các thuộc tính phải có getter và setter
+            public string TenNCC { get; set; }
+            public string DiaChi { get; set; }
+            public string SDT { get; set; }
+            public string Email { get; set; }
+            public string MoTa { get; set; }
+        }
+
+        // Hàm LoadData sẽ được gọi để thêm dữ liệu vào DataGrid
+        private void LoadData()
+        {
+            // Tạo ObservableCollection chứa dữ liệu mẫu
+            ObservableCollection<NhaCungCapModel> nhaCungCapList = new ObservableCollection<NhaCungCapModel>
+            {
+                new NhaCungCapModel { MaNCC = "001", TenNCC = "Công ty A", DiaChi = "Hà Nội", SDT = "0987654321", Email = "a@company.com", MoTa = "Cung cấp vật liệu" },
+                new NhaCungCapModel { MaNCC = "002", TenNCC = "Công ty B", DiaChi = "Hồ Chí Minh", SDT = "0123456789", Email = "b@company.com", MoTa = "Cung cấp thiết bị" }
+            };
+
+            // Gán dữ liệu cho DataGrid
+            supplierDataGrid.ItemsSource = nhaCungCapList;
+        }
+
+        private void btnThemMoi_Click(object sender, RoutedEventArgs e)
+        {
+            ThemNhaCungCapForm frmThemNCC = new ThemNhaCungCapForm();   
+            frmThemNCC.ShowDialog();
         }
     }
-
-    
-
 }
