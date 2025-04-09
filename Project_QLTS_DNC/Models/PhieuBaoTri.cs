a1f0629ccdace3cb88580f8ac7ddd00224062e21
Hao Nam <Hao Nam@DESKTOP-EE4R1MJ>
2025-04-10 00:07:09 +0700

    push code

--- Project_QLTS_DNC/Models/PhieuBaoTri.cs
+++ Project_QLTS_DNC/Models/PhieuBaoTri.cs
@@ -5,11 +5,20 @@ namespace Project_QLTS_DNC.Models
 {
     public class PhieuBaoTri : INotifyPropertyChanged
     {
+        private int _maBaoTri;
+        private int? _maTaiSanId;
+        private int? _maLoaiBaoTri;
+        private DateTime? _ngayBaoTri;
+        private int? _maNv;
+        private string _noiDung;
+        private string _trangThaiSauBaoTri;
+        private decimal? _chiPhi;
+        private string _ghiChu;
+
         private string _maPhieu;
         private string _maTaiSan;
         private string _tenTaiSan;
         private string _loaiBaoTri;
-        private DateTime _ngayBaoTri;
         private DateTime? _ngayHoanThanh;
         private string _nguoiPhuTrach;
         private string _trangThai;
@@ -19,113 +28,195 @@ namespace Project_QLTS_DNC.Models
 
         public event PropertyChangedEventHandler PropertyChanged;
 
+        // Các thuộc tính chính từ cơ sở dữ liệu
+        public int MaBaoTri
+        {
+            get => _maBaoTri;
+            set
+            {
+                _maBaoTri = value;
+                OnPropertyChanged(nameof(MaBaoTri));
+            }
+        }
+
+        public int? MaTaiSanId
+        {
+            get => _maTaiSanId;
+            set
+            {
+                _maTaiSanId = value;
+                OnPropertyChanged(nameof(MaTaiSanId));
+            }
+        }
+
+        public int? MaLoaiBaoTri
+        {
+            get => _maLoaiBaoTri;
+            set
+            {
+                _maLoaiBaoTri = value;
+                OnPropertyChanged(nameof(MaLoaiBaoTri));
+            }
+        }
+
+        public DateTime? NgayBaoTri
+        {
+            get => _ngayBaoTri;
+            set
+            {
+                _ngayBaoTri = value;
+                OnPropertyChanged(nameof(NgayBaoTri));
+            }
+        }
+
+        public int? MaNv
+        {
+            get => _maNv;
+            set
+            {
+                _maNv = value;
+                OnPropertyChanged(nameof(MaNv));
+            }
+        }
+
+        public string NoiDung
+        {
+            get => _noiDung;
+            set
+            {
+                _noiDung = value;
+                OnPropertyChanged(nameof(NoiDung));
+            }
+        }
+
+        public string TrangThaiSauBaoTri
+        {
+            get => _trangThaiSauBaoTri;
+            set
+            {
+                _trangThaiSauBaoTri = value;
+                OnPropertyChanged(nameof(TrangThaiSauBaoTri));
+            }
+        }
+
+        public decimal? ChiPhi
+        {
+            get => _chiPhi;
+            set
+            {
+                _chiPhi = value;
+                OnPropertyChanged(nameof(ChiPhi));
+            }
+        }
+
+        public string GhiChu
+        {
+            get => _ghiChu;
+            set
+            {
+                _ghiChu = value;
+                OnPropertyChanged(nameof(GhiChu));
+            }
+        }
+
+        // Các thuộc tính hiển thị hoặc kết hợp từ bảng khác
         public string MaPhieu
         {
-            get { return _maPhieu; }
+            get => _maPhieu;
             set
             {
                 _maPhieu = value;
-                OnPropertyChanged("MaPhieu");
+                OnPropertyChanged(nameof(MaPhieu));
             }
         }
 
         public string MaTaiSan
         {
-            get { return _maTaiSan; }
+            get => _maTaiSan;
             set
             {
                 _maTaiSan = value;
-                OnPropertyChanged("MaTaiSan");
+                OnPropertyChanged(nameof(MaTaiSan));
             }
         }
 
         public string TenTaiSan
         {
-            get { return _tenTaiSan; }
+            get => _tenTaiSan;
             set
             {
                 _tenTaiSan = value;
-                OnPropertyChanged("TenTaiSan");
+                OnPropertyChanged(nameof(TenTaiSan));
             }
         }
 
         public string LoaiBaoTri
         {
-            get { return _loaiBaoTri; }
+            get => _loaiBaoTri;
             set
             {
                 _loaiBaoTri = value;
-                OnPropertyChanged("LoaiBaoTri");
-            }
-        }
-
-        public DateTime NgayBaoTri
-        {
-            get { return _ngayBaoTri; }
-            set
-            {
-                _ngayBaoTri = value;
-                OnPropertyChanged("NgayBaoTri");
+                OnPropertyChanged(nameof(LoaiBaoTri));
             }
         }
 
         public DateTime? NgayHoanThanh
         {
-            get { return _ngayHoanThanh; }
+            get => _ngayHoanThanh;
             set
             {
                 _ngayHoanThanh = value;
-                OnPropertyChanged("NgayHoanThanh");
+                OnPropertyChanged(nameof(NgayHoanThanh));
             }
         }
 
         public string NguoiPhuTrach
         {
-            get { return _nguoiPhuTrach; }
+            get => _nguoiPhuTrach;
             set
             {
                 _nguoiPhuTrach = value;
-                OnPropertyChanged("NguoiPhuTrach");
+                OnPropertyChanged(nameof(NguoiPhuTrach));
             }
         }
 
         public string TrangThai
         {
-            get { return _trangThai; }
+            get => _trangThai;
             set
             {
                 _trangThai = value;
-                OnPropertyChanged("TrangThai");
+                OnPropertyChanged(nameof(TrangThai));
             }
         }
 
         public decimal ChiPhiDuKien
         {
-            get { return _chiPhiDuKien; }
+            get => _chiPhiDuKien;
             set
             {
                 _chiPhiDuKien = value;
-                OnPropertyChanged("ChiPhiDuKien");
+                OnPropertyChanged(nameof(ChiPhiDuKien));
             }
         }
 
         public string NoiDungBaoTri
         {
-            get { return _noiDungBaoTri; }
+            get => _noiDungBaoTri;
             set
             {
                 _noiDungBaoTri = value;
-                OnPropertyChanged("NoiDungBaoTri");
+                OnPropertyChanged(nameof(NoiDungBaoTri));
             }
         }
 
         public string DonViBaoTri
         {
-            get { return _donViBaoTri; }
+            get => _donViBaoTri;
             set
             {
                 _donViBaoTri = value;
-                OnPropertyChanged("DonViBaoTri");
+                OnPropertyChanged(nameof(DonViBaoTri));
             }
         }
 
