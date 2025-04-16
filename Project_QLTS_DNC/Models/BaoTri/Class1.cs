97fb0dc864c2d76c548ba078fb58808e48fac557
Hao Nam <Hao Nam@DESKTOP-EE4R1MJ>
2025-04-16 14:54:37 +0700

    push

---
+++ Project_QLTS_DNC/Models/BaoTri/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_QLTS_DNC.Models.BaoTri
+{
+    internal class Class1
+    {
+    }
+}
