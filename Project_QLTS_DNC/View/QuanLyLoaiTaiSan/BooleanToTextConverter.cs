dcbcde80fda78369467374d76f35080137638f24
thanhdat-24 <132144790+thanhdat-24@users.noreply.github.com>
2025-04-18 15:47:14 +0700

    up code thêm loại ts, nhóm ts

---
+++ Project_QLTS_DNC/View/QuanLyLoaiTaiSan/BooleanToTextConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Project_QLTS_DNC.View.QuanLyTaiSan
+{
+    public class BooleanToTextConverter : IValueConverter
+    {
+        public string TrueText { get; set; } = "Có";
+        public string FalseText { get; set; } = "Không";
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return boolValue ? TrueText : FalseText;
+            }
+            return FalseText;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string stringValue)
+            {
+                return stringValue == TrueText;
+            }
+            return false;
+        }
+    }
+}
